ccb74dd4ca223f02aa8ce7e21a4d05e72ccd91df
bekiassefa <bereketassefaalx@gmail.com>
2022-12-14 13:54:23 -0800

    task 12

--- 0x02-functions_nested_loops/100-main.c
+++ 0x02-functions_nested_loops/100-main.c
@@ -5,15 +5,14 @@
  *
  * Return: Always 0.
  */
-
 int main(void)
 {
-print_times_table(3);
-_putchar('\n');
-print_times_table(5);
-_putchar('\n');
-print_times_table(98);
-_putchar('\n');
-print_times_table(12);
-return (0);
+	print_time_table(3);
+	_putchar('\n');
+	print_time_table(5);
+	_putchar('\n');
+	print_time_table(98);
+	_putchar('\n');
+	print_time_table(12);
+	return (0);
 }
