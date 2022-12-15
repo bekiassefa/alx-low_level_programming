8e99578d41c3277b62b7f679103fb20214597453
bekiassefa <bereketassefaalx@gmail.com>
2022-12-15 12:17:06 -0800

    task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,11 +2,13 @@
 
 /**
  * print_most_numbers - Prints numbers "0" to "9", except 2 and 4
+ *
  * Return: is zero
  */
 void print_most_numbers(void)
 {
 	char digits;
+
 	for (digits = '0'; digits <= '9'; digits++)
 	{
 		if (digits != '9' && digits != '4')
