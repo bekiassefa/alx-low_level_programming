bcf982527dcccb8180a91f4b2c7f59cd84c021af
bekiassefa <bereketassefaalx@gmail.com>
2022-12-19 11:45:55 -0800

    task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -15,19 +15,17 @@ void print_rev(char *s)
 	_putchar('\n');
 }
 
-
 /**
  * _strlen - returns the length of a string
  * @s: string
  * Return: returns lenght;
  */
-
 int _strlen(char *s)
-
 {
 	int count, inc;
 	inc = 0;
 	for (count = 0; s[count] != '\0'; count++)
 		inc++;
+
 	return (inc);
 }
