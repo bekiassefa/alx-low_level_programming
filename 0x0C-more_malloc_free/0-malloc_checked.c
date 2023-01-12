33fb860c2ead8285c0c59d3c3fb04d885e5504e4
bekiassefa <bereketassefaalx@gmail.com>
2023-01-12 04:25:16 -0800

    test 0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -6,10 +6,12 @@
  *
  * Return: pointer to the address of the memory block
  */
-void *malloc_checked(unsigned int b) {
-    void *ptr = malloc(b);
-    if (ptr == NULL) {
-        exit(98);
-    }
-    return ptr;
+void *malloc_checked(unsigned int b)
+{
+	void *block;
+
+	block = malloc(b);
+	if (block == NULL)
+		exit(98);
+	return (block);
 }
